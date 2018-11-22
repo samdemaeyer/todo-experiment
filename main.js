9742b59e3ac07179da0b07926cd3ea7d5d3b2a71
chrisbaidoo <chris.baidoo@smartpension.co.uk>
2018-11-22 16:43:26 +0000

    New work

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+function toggleToDo(element){
+  var newClass;
+  var completed = element.querySelector(".fa").classList.contains("fa-check-square");
+  if (completed) {
+    newClass = element.querySelector(".fa").className.replace("fa-check-square", "fa-square-o");
+  } else {
+    newClass = element.querySelector(".fa").className.replace("fa-square-o", "fa-check-square");
+  }
+  element.querySelector(".fa").className = newClass;
+}
