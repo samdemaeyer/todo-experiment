765a258a352285ada5940e42fcc8e6b305c0a72a
chrisbaidoo <chris.baidoo@smartpension.co.uk>
2018-11-27 07:35:51 +0000

    Changed var to const

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function toggleToDo(element){
-  var newClass;
-  var completed = element.querySelector(".fa").classList.contains("fa-check-square");
+  const newClass;
+
+  const completed = element.querySelector(".fa").classList.contains("fa-check-square");
+
   if (completed) {
     newClass = element.querySelector(".fa").className.replace("fa-check-square", "fa-square-o");
   } else {
