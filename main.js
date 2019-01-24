a201ea36797eec3f06e6285439750a5c0a790027
chrisbaidoo <chris.baidoo@smartpension.co.uk>
2019-01-24 14:16:50 +0000

    make transitions workt

--- main.js
+++ main.js
@@ -19,36 +19,42 @@ function toggleToDo(element){
   faClassName.classList.toggle('fa-square-o');
 
   document.getElementById(listWrapperId).appendChild(element);
+  element.classList.add('transition-in');
+  function transition(timeoutID){
+    element.classList.remove('transition-in');
+    window.clearTimeout(timeoutID);
+  }
+  const timeoutID = window.setTimeout(() => transition(timeoutID), 100);
 }
 
-const toDoItems = [
-  {name: 'Todo 1', completed:false},
-  {name: 'Todo 2', completed:false},
-  {name: 'Todo 3', completed:false},
-  {name: 'Todo 4', completed:true},
-  {name: 'Todo 5', completed:true},
-  {name: 'Todo 6', completed:true},
+const toDoItems = [ //Create an array of 6 objects with a flag completed or not completed
+  { name: 'Todo 1', completed:false },
+  { name: 'Todo 2', completed:false },
+  { name: 'Todo 3', completed:false },
+  { name: 'Todo 4', completed:true },
+  { name: 'Todo 5', completed:true },
+  { name: 'Todo 6', completed:true },
 ];
 
 toDoItems.forEach(function(toDoItem) {
-  let listWrapperId = 'not-completed';
-  let iconClass = 'fa-square-o';
-  if (toDoItem.completed) {
+  let listWrapperId = 'not-completed'; //Set default ids for the wrapper of completed or not completed list items
+  let iconClass = 'fa-square-o';//Set default icon class for completed or not completed item
+  if (toDoItem.completed) { //Loop through the array to overwite the defaults if needed
     listWrapperId = 'completed';
     iconClass = 'fa-check-square';
   }
 
-
-  const newListItem = document.createElement('li');
-  newListItem.classList.add('item');
+  const newListItem = document.createElement('li'); //Create a new li element
+  newListItem.classList.add('item'); //Add a class called 'item'
   newListItem.setAttribute('data-completed', toDoItem.completed);
-  newListItem.setAttribute('onclick', 'toggleToDo(this)');
-  const newIcon = document.createElement('i');
-  newIcon.classList.add(iconClass);
-  newIcon.classList.add('fa');
-  const newListTitle = document.createElement('span');
-  newListTitle.innerText = toDoItem.name;
-  newListItem.appendChild(newIcon);
-  newListItem.appendChild(newListTitle);
+  newListItem.setAttribute('onclick', 'toggleToDo(this)');//set onclick attribute and assign toggleToDo(this)
+  const newIcon = document.createElement('i');//Create a new i tag
+  newIcon.classList.add(iconClass);//Add a the 'iconClass' class  to the i tag
+  newIcon.classList.add('fa');//Add a the 'fa' class  to the i tag
+  const newListTitle = document.createElement('span');//Create a span
+  newListTitle.innerText = toDoItem.name;//Add the name of the toDoItem to the span
+  newListItem.appendChild(newIcon);//Add the icon to newListItem
+  newListItem.appendChild(newListTitle);//Add the span to newListItem
   document.getElementById(listWrapperId).appendChild(newListItem);
+  //Get all the newListItems(which can be either completed or not completed) add all the newListItems
 });
